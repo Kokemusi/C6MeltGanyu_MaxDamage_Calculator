f5cb5b1798766632d389c96b43335932fd0c3939
Kokemusi <74450392+Kokemusi@users.noreply.github.com>
2024-03-28 10:32:13 +0900

    Update simulator.js

--- simulator.js
+++ simulator.js
@@ -617,6 +617,7 @@ function calculate(){
 	let sandsid = document.getElementById("Sands");
 	let resultid = document.getElementById("result");
 	let setid = document.getElementById("Artifact");
+	console.log(artifact_set[setid.value]);
 	WeaponName=weaponid.value;
 	OptimizeSub(WeaponName,sandsid.value,setid.value);
 	let i = 19;
